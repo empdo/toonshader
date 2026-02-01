d5be4ff9e632ba0b5da8dded2d56fc22d1cb21ca
lomstfer <90508408+lomstfer@users.noreply.github.com>
2026-02-01 15:06:11 +0100

    vignetto

--- scripts/vignette_thing.gd
+++ scripts/vignette_thing.gd
@@ -6,17 +6,10 @@ var mat = material as ShaderMaterial
 func _ready():
 	var mat = material as ShaderMaterial
 	start_radius = mat.get_shader_parameter("vignette_radius")
-	Globals.see_through_cards.connect(on_see)
 
-var see = false
-func on_see(seee):
-	see = seee
 
 func _process(delta):
 	var v
-	if see:
-		v = Globals.time_used_seeingmask / Globals.max_time_used_seeingmask_until_collapse
-		v = lerp(start_radius, 0.0, v)
-	else:
-		v = start_radius
+	v = Globals.time_used_seeingmask / Globals.max_time_used_seeingmask_until_collapse
+	v = lerp(start_radius, 0.0, v)
 	mat.set_shader_parameter("vignette_radius", v)
