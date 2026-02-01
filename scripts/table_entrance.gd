0ec34b7f7b841f0005e848c5ada6447936707d98
Alve Svarén <alvel@student.chalmers.se>
2026-02-01 16:03:16 +0100

    Fix triggers finally

--- scripts/table_entrance.gd
+++ scripts/table_entrance.gd
@@ -6,6 +6,7 @@ extends Area3D
 var last_entered = 0
 var player_in_area: Node3D = null
 var waiting_for_approach_dialog: bool = true
+var has_sat_down: bool = false
 
 func _ready():
 	Globals.won_game.connect(on_won)
@@ -17,8 +18,7 @@ func on_won():
 
 func _on_dialog_finished(dialog: DialogResource) -> void:
 	# Check if the finished dialog is the approach_table dialog
-	var approach_dialog = load("res://resources/b_approach_table.tres") as DialogResource
-	if dialog == approach_dialog or dialog.resource_path == "res://resources/b_approach_table.tres":
+	if dialog.resource_path == "res://resources/b_approach_table.tres":
 		waiting_for_approach_dialog = false
 		# If player is already in the area, trigger sit down immediately
 		if player_in_area and player_in_area.is_in_group("player"):
@@ -36,6 +36,9 @@ func _on_body_exited(body: Node3D) -> void:
 		player_in_area = null
 
 func _trigger_sit_down() -> void:
+	if has_sat_down:
+		return
+	has_sat_down = true
 	if player_target and camera_target:
 		Globals.player_entered_table_area_with_targets.emit(player_target, camera_target)
 		Globals.player_entered_table_area.emit()
